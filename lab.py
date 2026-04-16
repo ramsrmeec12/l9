c5d5408652bb279b5e927d6e8dd56720082993f7
Ramkumar <ramskv1018@gmail.com>
2026-04-16 10:58:02 +0530

    Update lab.py

--- lab.py
+++ lab.py
@@ -1,18 +1,17 @@
+
 import cv2
-image = cv2.imread(&quot;input_image.jpg&quot;)
-if image is None:
-print(&quot;Error: Image not loaded&quot;)
-exit()
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cascade = cv2.CascadeClassifier(&quot;haarcascade_frontalface_default.xml&quot;)
-objects = cascade.detectMultiScale(
-gray,
-scaleFactor=1.1,
-minNeighbors=5,
-minSize=(30, 30)
-)
-for (x, y, w, h) in objects:
-cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-cv2.imshow(&quot;Object Detection Result&quot;, image)
+
+img = cv2.imread("download.jpeg")
+if img is None: exit()
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")\
+        .detectMultiScale(gray, 1.1, 5)
+
+for (x,y,w,h) in faces:
+    cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+
+cv2.imshow("Result", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
